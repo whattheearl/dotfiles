d1f77a24d2b3fae8b4c5d89c737465fb70fb2ed9
Jonathan Earl <jonathan.earl@tylertech.com>
2024-09-29 14:27:03 -0700

    (wezterm): remove padding

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,8 +4,8 @@ local config = {}
 config.default_prog = { "tmux" }
 config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = 2,
-  right = 2,
+  left = 0,
+  right = 0,
   top = 0,
   bottom = 0,
 }
