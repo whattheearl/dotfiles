a90bd32632626b8d66d5c6faa86c18412d37107b
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-01 07:45:52 -0700

    (nvim): make oil non-default

--- config/nvim/lua/custom/plugins/oil.lua
+++ config/nvim/lua/custom/plugins/oil.lua
@@ -6,6 +6,7 @@ return {
   opts = {},
   -- Optional dependencies
   config = {
+    default_file_explorer = false,
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
   },
 }
