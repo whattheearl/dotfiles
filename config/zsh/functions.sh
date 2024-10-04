fd512e1060fd60a65431ccc02e525008af07cc4b
jonathan earl <jonathan.earl@tylertech.com>
2024-10-03 18:37:23 -0700

    (zsh): add change session function

--- config/zsh/functions.sh
+++ config/zsh/functions.sh
@@ -2,6 +2,17 @@
 # settings > keyboard > keyboard shortcuts > custom shortcuts
 # alacritty -e /home/jon/wte/docs/scripts/find-project.sh
 
+function cds() {
+  project_path="$(find ~ -maxdepth 2 -type d -path '/home/jon/wte/**' -or -path '/home/jon/tyl/**' -not -path '*/.cache*' | fzf)"
+  session_name="$(basename $project_path)"
+  if [ "$TERM_PROGRAM" != "tmux" ]; then
+    tmux attach -t $session_name || tmux new -s $session_name -c $project_path
+  else
+    tmux new -s $session_name -d -c $project_path &> /dev/null
+    tmux switch-client -t $session_name
+  fi
+}
+
 function caps-to-control() {
   gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
   setxkbmap -option caps:ctrl_modifier 
@@ -20,3 +31,5 @@ function open-project-session() {
     tmux a
   fi
 }
+
+
