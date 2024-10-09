4227496b4cbc67f8515d7573062c74f5fc49c833
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-09 08:51:04 -0700

    (zsh): poc alias

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,3 +1,4 @@
 alias tcp="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml $@"
+alias poc="docker compose -f ~/tyl/poc-node-bff/docker-compose/platform-dev-environment-compose//docker-compose.yml $@"
 alias tid="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml -f ~/tyl/platform-dev-environment-compose/docker-compose.tid.yml $@"
 
