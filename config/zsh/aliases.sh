4efff93c47f489fa2037c0cb4c18a20ebe72516a
Jonathan Earl <jonathan.earl@tylertech.com>
2024-10-10 07:19:35 -0700

    (zsh): alias nvim

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,4 +1,6 @@
 alias tcp="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml $@"
 alias poc="docker compose -f ~/tyl/poc-node-bff/docker-compose/platform-dev-environment-compose//docker-compose.yml $@"
 alias tid="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml -f ~/tyl/platform-dev-environment-compose/docker-compose.tid.yml $@"
+alias vim=nvim $@
+alias vi=nvim $@
 
