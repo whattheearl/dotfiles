6fa31c14814747f218eb4cb022ebbbacb5f671f6
jonathan earl <jonathan.earl@tylertech.com>
2024-10-21 15:58:47 -0700

    add csharp lsp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -609,6 +609,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        csharp_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
