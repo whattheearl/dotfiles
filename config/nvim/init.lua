f050011f84b0b6bdca6265d7da3d571135b55248
Jonathan Earl <jonathan.earl@tylertech.com>
2024-11-13 10:55:30 -0800

    (nvim) fix conform errors

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -732,12 +732,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        ['markdown'] = { 'prettier', 'markdownlint-cli2', 'markdown-toc' },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { { 'prettier', 'markdownlint-cli2', 'markdown-toc' } },
+        javascript = { 'prettier' },
       },
     },
   },
