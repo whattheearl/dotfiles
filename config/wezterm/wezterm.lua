073c0cc339e8b161060a11fdaa850701ea287199
Jonathan Earl <jonathan.earl@tylertech.com>
2024-11-12 23:43:59 -0800

    (wezterm) remove overlapping i3 keys

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,13 +4,31 @@ local config = {}
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
 
 config.warn_about_missing_glyphs = false
 config.color_scheme = "Google Dark (Gogh)"
 
+config.keys = {
+    -- unbind SUPER keys
+    {
+        key = "-",
+        mods = "SUPER",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = "+",
+        mods = "SUPER",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = "0",
+        mods = "SUPER",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+}
 return config
