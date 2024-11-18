b2b24a0627ac7e8eed21bb8fb6aafa3fbed7b643
Jonathan Earl <jonathan.earl@tylertech.com>
2024-11-17 17:37:11 -0800

    tired of typing docker comopse

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,11 +1,11 @@
 alias tcp="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml $@"
 alias tid="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml -f ~/tyl/platform-dev-environment-compose/docker-compose.tid.yml $@"
 alias bsc="docker compose -f ~/tyl/v2brandingservice/docker-compose.yml $@"
+alias dc="docker compose $@"
 
 alias v=nvim $@
 alias config="cd ~/wte/docs && v ~/wte/docs"
 alias n="cd ~/wte/notes && v ~/wte/notes/today.md"
-alias bs="cd ~/tyl/v2brandingservice && tmux"
 
 alias cds=$HOME/wte/docs/scripts/sessionizer.sh $@
 alias devd=$HOME/tyl/dev-cli/src/index.ts $@
