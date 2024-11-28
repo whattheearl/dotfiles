c061ad36a1cfe4ba7313ff3bc1f06076cef10650
Jonathan Earl <jonathan.earl@tylertech.com>
2024-11-28 10:10:42 -0800

    (dotnet) add kiota

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,14 +1,28 @@
+# vim
+alias v=nvim $@
+
+# docker
 alias dc="docker compose $@"
 
+# platform
 alias tcp="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml $@"
 alias tid="docker compose -f ~/tyl/platform-dev-environment-compose/docker-compose.yml -f ~/tyl/platform-dev-environment-compose/docker-compose.tid.yml $@"
 
+# branding-service
 alias bs="make -C ~/tyl/v2brandingservice $@"
 alias bsc="docker compose -f ~/tyl/v2brandingservice/docker-compose.yml $@"
 
-alias v=nvim $@
-alias config="cd ~/wte/docs && v ~/wte/docs"
+# dotnet 
+alias kiota="${HOME}/.dotnet/tools/kiota $@"
+
+# notes
 alias n="cd ~/wte/notes && v ~/wte/notes/today.md"
 
+# tmux-session
 alias cds=$HOME/wte/docs/scripts/sessionizer.sh $@
+
+# dev-cli
 alias devd=$HOME/tyl/dev-cli/src/index.ts $@
+
+# jeddit
+alias jed="make -C ${HOME}/wte/jeddit"
