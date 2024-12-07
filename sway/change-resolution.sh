67be2d7cd71fc18ee7952a6847de6a49bab975f7
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-07 12:21:46 -0800

    allow no choice

--- sway/change-resolution.sh
+++ sway/change-resolution.sh
@@ -1,7 +1,8 @@
 choice=$(printf '%s\n' 3840x2160 1920x1080 | wofi --show=dmenu)
 
+
 if [ $choice = "3840x2160" ]; then
     swaymsg output DP-3 pos 0 0 res 3840x2160 scale 2
-else
+else if [ $choice = "1920x1080" ]
     swaymsg output DP-3 pos 0 0 res 1920x1080 scale 1
 fi
