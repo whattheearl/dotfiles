144c2b8111dafe1f8d0e0657a4c2522bbd3b77eb
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-08 10:55:33 -0800

    rename folder

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -sf ${PWD}/nvim ${HOME}/.config
+ln -sf ${PWD}/wezterm ${HOME}/.config
+ln -sf ${PWD}/tmux ${HOME}/.config
+ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
+ln -sf ${PWD}/zsh ${HOME}/.config
+ln -sf ${PWD}/sway ${HOME}/.config
+ln -sf ${PWD}/waybar ${HOME}/.config
+ln -sf ${PWD}/wofi ${HOME}/.config
