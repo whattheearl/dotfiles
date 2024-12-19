390f9814a96a8119e06baf6c45c6b318917a9fcd
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-19 14:37:13 -0800

    reduce font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,11 @@
 local wezterm = require("wezterm")
 local config = {}
+
 config.debug_key_events = true
+
 config.hide_tab_bar_if_only_one_tab = true
+
+config.font_size = 11.0
 
 config.window_padding = {
     left = 0,
