06ba7381e07a0d7094b351a20eb6eba68f624f00
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-21 09:00:21 -0800

    (term) font back to 12

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,68 +5,24 @@ config.debug_key_events = true
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font_size = 11.0
+config.font_size = 12.0
 
-config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-}
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.warn_about_missing_glyphs = false
 
 -- config.color_scheme = "Google Dark (Gogh)"
 
 config.keys = {
-    {
-        key = "-",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "+",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "t",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "0",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "n",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "f",
-        mods = "SUPER",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "Tab",
-        mods = "CTRL",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-        key = "Tab",
-        mods = "SHIFT|CTRL",
-        action = wezterm.action.DisableDefaultAssignment,
-    },
+    { key = "-", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "+", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "t", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "0", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "n", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "f", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+    { key = "Tab", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
 }
 
-config.skip_close_confirmation_for_processes_named = {
-    "flatpak-spawn",
-    "bash",
-    "sh",
-    "zsh",
-    "tmux",
-}
+config.skip_close_confirmation_for_processes_named = { "flatpak-spawn", "bash", "sh", "zsh", "tmux" }
 
 return config
