781ad9b79b3138a9728c21bcba62329309399488
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-22 13:01:57 -0800

    smaller fonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.debug_key_events = true
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font_size = 12.0
+config.font_size = 11.0
 
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
