09cfc28063d187af10c4c34f3d441eaf26437499
Jonathan Earl <jonathan.earl@tylertech.com>
2024-12-22 14:45:03 -0800

    (wezterm) default tmux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 
+config.default_prog = { "/usr/bin/tmux", "new-session", "-A", "-s default" }
 config.debug_key_events = true
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -10,8 +11,6 @@ config.font_size = 11.0
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 config.warn_about_missing_glyphs = false
-
--- config.color_scheme = "Google Dark (Gogh)"
 
 config.keys = {
     { key = "-", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
