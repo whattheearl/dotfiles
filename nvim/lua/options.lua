92ec0b064227b3b7affbaf158f9d8786e39c2055
Jonathan Earl <jonathan.earl@tylertech.com>
2025-01-02 16:08:14 -0800

    format

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,23 +1,23 @@
+vim.opt.breakindent = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.list = false
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.modeline = false
+vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
+vim.opt.scrolloff = 10
 vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.updatetime = 250
 vim.schedule(function()
     vim.opt.clipboard = "unnamedplus"
 end)
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.inccommand = "split"
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-vim.opt.modeline = false
