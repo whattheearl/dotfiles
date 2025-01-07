72bf6446a5c448a6b8b4b469254ae3c41687bbf8
jon <jon@ws.wte.sh>
2025-01-06 18:30:23 -0800

    remove unused

--- zsh/source.sh
+++ zsh/source.sh
@@ -10,8 +10,6 @@ source $ZSH/oh-my-zsh.sh
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-. "$HOME/.cargo/env"
-. "$HOME/.deno/env"
 
 # autocompletion
 autoload -Uz compinit
