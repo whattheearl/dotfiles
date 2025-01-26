e84b52796918b40e513249bd235ab210ec87f294
jon <jon@jons-MacBook-Air.local>
2025-01-26 11:51:44 -0800

    add osx paths

--- zsh/source.sh
+++ zsh/source.sh
@@ -1,12 +1,21 @@
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
 export ZSH=$HOME/.oh-my-zsh
+
+export DOTNET_CLI_TELEMETRY_OPTOUT='true'
+export PATH="$PATH:/Users/jon/.dotnet/tools"
+
 ZSH_THEME="powerlevel10k/powerlevel10k"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)
 source $ZSH/oh-my-zsh.sh
+
 [ -s "/home/jon/.bun/_bun" ] && source "/home/jon/.bun/_bun"
+[ -s "/Users/jon/.bun/_bun" ] && source "/Users/jon/.bun/_bun"
+
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
