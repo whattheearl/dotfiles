67b4945b56c7856b35ea1a930f5e74677a799b0a
jonathan earl <jonathan.earl@tylertech.com>
2025-04-18 09:46:50 -0700

    (nvim) enable eslint

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -224,6 +224,7 @@ return {
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        eslint_d = {},
         ts_ls = {},
         svelte = {
           capabilities = {
--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -10,10 +10,10 @@ require("lazy").setup({
   require("kickstart/plugins/mini"),
   require("kickstart/plugins/treesitter"),
   -- require 'kickstart.plugins.debug',
-  require("kickstart.plugins.indent_line"),
-  -- require 'kickstart.plugins.lint',
-  require("kickstart.plugins.autopairs"),
-  require("kickstart.plugins.neo-tree"),
+  require("kickstart/plugins/indent_line"),
+  require("kickstart/plugins/lint"),
+  require("kickstart/plugins/autopairs"),
+  require("kickstart/plugins/neo-tree"),
   { import = "custom.plugins" },
 }, {
   ui = {
