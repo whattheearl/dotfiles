9db52cace968b797e7e60e5d5f70d90b56e1a158
jonathan earl <jonathan.earl@tylertech.com>
2025-04-22 19:19:32 -0700

    (nvim) add json formatter

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -266,6 +266,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "csharpier",
+        "prettier",
         "prettierd",
         "stylua", -- Used to format Lua code
       })
