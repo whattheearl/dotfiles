05da8e61c9258a520ba35395fdd5f893c55fd0ba
jonathan earl <jonathan.earl@tylertech.com>
2025-04-30 20:15:05 -0700

    (nvim) update packages

--- nvim/lua/kickstart/plugins/blink-cmp.lua
+++ nvim/lua/kickstart/plugins/blink-cmp.lua
@@ -24,17 +24,7 @@ return {
           }
         end,
 
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
+        dependencies = {},
         opts = {},
       },
       'folke/lazydev.nvim',
