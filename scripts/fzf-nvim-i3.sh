26f0b268fe976ac0ec482fdf12b9203ff3448fa9
jonathan earl <jonathan.earl@tylertech.com>
2025-05-16 16:16:39 -0700

    (scripts) update again?

--- scripts/fzf-nvim-i3.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# INFO: temp solution to keep zsh open after opening from i3
-
-folder=$(fd "./*" --type directory --max-depth 1 ${HOME}/tyl ${HOME}/wte | fzf);
-cd ${folder}
-nvim ${folder};
-zsh;
--- scripts/fzf-nvim.sh
+++ scripts/fzf-nvim.sh
@@ -2,5 +2,6 @@
 
 # INFO: wezterm start -- zsh -i -e ~/script
 folder=$(fd "./*" --type directory --max-depth 1 ${HOME}/tyl ${HOME}/wte | fzf);
-cd ${folder}
-nvim ${folder};
+if [ -n "${folder}" ]; then
+    cd ${folder}
+fi
