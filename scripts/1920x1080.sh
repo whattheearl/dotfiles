2498094109aced2014d5d948a38b36b2ff267a65
jonathan earl <jonathan.earl@tylertech.com>
2025-05-25 09:02:04 -0700

    (scripts) fix status bar size

--- scripts/1920x1080.sh
+++ scripts/1920x1080.sh
@@ -3,3 +3,4 @@
 echo "Xft.dpi: 96" > ${HOME}/.Xresources
 xrdb -merge ~/.Xresources
 xrandr --output DisplayPort-2 --mode 1920x1080
+i3-msg restart
--- scripts/3840x2160.sh
+++ scripts/3840x2160.sh
@@ -3,3 +3,4 @@
 echo "Xft.dpi: 192" > ${HOME}/.Xresources
 xrdb -merge ~/.Xresources
 xrandr --output DisplayPort-2 --mode 3840x2160
+i3-msg restart
