31ce899924646244d6d6b4df7695f0801756eb1d
jonathan earl <jonathan.earl@tylertech.com>
2025-09-15 08:54:38 -0700

    (setup) add hint

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# curl -s https://raw.githubusercontent.com/whattheearl/dotfiles/main/setup.sh | bash
+
 sudo pacman -S git
 git clone https://github.com/whattheearl/dotfiles /tmp/dotfiles
 cd /tmp/dotfiles
