e2a91cc1f90fe6fb3ade2f9978e8ab21b5bcfea9
jonathan earl <earl.jonathan@gmail.com>
2026-03-12 16:35:32 -0700

    (nvim/fzf-lua): exclude package-lock.json and *.age files from grep search
    Add glob patterns to rg_opts to ignore package-lock.json and encrypted *.age files
    when searching with fzf-lua's grep functionality.

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -2,6 +2,9 @@ return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-mini/mini.icons" },
   opts = {
+    grep = {
+      rg_opts = "--column --line-number --no-heading --color=always --smart-case --glob '!package-lock.json' --glob '!*.age'"
+    },
     files = {
       winopts = {
         preview = {
