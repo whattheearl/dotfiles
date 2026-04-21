3936df4a0b146f1ad904557e8330ec3724b91a14
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 11:31:33 -0700

    chore(nix): simplify flake structure and add lockfile
    - Initialize `flake.lock` to pin dependencies
    - Simplify `flake.nix` by removing the `devShell` configuration
    - Define `packages.hello` and set it as the default package for `x86_64-linux`
    - Update `nixpkgs` input reference format

--- flake.nix
+++ flake.nix
@@ -1,24 +1,15 @@
 {
-  description = "A basic flake for a development shell";
+  description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.hello
-        ];
+  outputs = { self, nixpkgs }: {
 
-        shellHook = ''
-          echo "Welcome to the dev shell"
-        '';
-      };
-    };
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
 }
