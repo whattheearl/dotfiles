a28a02ff300c4e4162dc1b801a64155c8f052c9f
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 11:36:49 -0700

    WIP

--- flake.nix
+++ flake.nix
@@ -9,7 +9,12 @@
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.x86_64-linux.default = {
+        self.packages.x86_64-linux = {
+            hello,
+            home-manager
+        }
+    }
     packages.x86_64-linux.default = self.packages.x86_64-linux.home-manager;
 
   };
