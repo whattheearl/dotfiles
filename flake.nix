f034210bcbf8dba9ccc3e6e55a2d242f79469df0
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 11:51:29 -0700

    WIP

--- flake.nix
+++ flake.nix
@@ -3,9 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      # We need to use nightly home-manager because it contains this
+      # fix we need for nushell nightly:
+      # https://github.com/nix-community/home-manager/commit/a69ebd97025969679de9f930958accbe39b4c705
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     packages.x86_64-linux = {
         default = self.packages.x86_64-linux.hello;
         hello = nixpkgs.legacyPackages.x86_64-linux.hello;
