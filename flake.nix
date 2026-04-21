0f9f8667c06f3d65dbbc2f39fa59f8bb77d540ef
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 11:24:31 -0700

    temp

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A basic flake for a development shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.hello
+        ];
+
+        shellHook = ''
+          echo "Welcome to the dev shell"
+        '';
+      };
+    };
+}
