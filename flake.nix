0e882345baaf19d438c0140424e19bbb13a04493
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 15:19:43 -0700

    WIP

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
-  description = "A very basic flake";
+    description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    packages.x86_64-linux = {
-        default = self.packages.x86_64-linux.home-manager;
-        home-manager = nixpkgs.legacyPackages.x86_64-linux.home-manager;
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
-  };
+
+    outputs = { self, nixpkgs, home-manager, ... }: {
+        packages = {
+            x86_64-linux = {
+                default = self.packages.x86_64-linux.home-manager;
+                home-manager = nixpkgs.legacyPackages.x86_64-linux.home-manager;
+            };
+            aarch64-linux = {
+                default = self.packages.aarch64-linux.home-manager;
+                home-manager = nixpkgs.legacyPackages.aarch64-linux.home-manager;
+            };
+        };
+    };
 
 }
