7be63c432000803eef9ef5b10e1a2a526e181e58
jonathan earl <earl.jonathan@gmail.com>
2026-04-21 09:23:57 -0700

    chore(setup): expand home-manager configuration and packages
    - Enable `unfree` packages and configure Nix User Repository (NUR)
    - Add several CLI tools including `ripgrep`, `fd`, `fzf`, `jq`, and `lazygit`
    - Configure `firefox` and `librewolf` with default extensions (uBlock Origin and Bitwarden)
    - Enable `yt-dlp` and reformat `home-manager` package definitions for better readability

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,24 +1,53 @@
 { config, pkgs, ... }:
 
-
 {
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+
   home.username = "jon";
   home.homeDirectory = "/home/jon";
   home.stateVersion = "26.05";
 
-  xdg.enable = true;
-
   home.packages = [
-    pkgs.chezmoi
+    pkgs.age
+    pkgs.bun
+    pkgs.cmake
+    pkgs.fd
+    pkgs.feh
+    pkgs.fzf
     pkgs.htop
+    pkgs.jq
+    pkgs.lazygit
+    pkgs.ripgrep
+    pkgs.rsync
   ];
 
-  xdg.configFile.nvim.source = ../nvim;
-
-  programs.home-manager.enable = true;
-  programs.neovim = {
-    enable = true;
+  xdg.enable = true;
+  xdg.configFile = {
+    nvim.source = ../nvim;
   };
+
+  programs.librewolf = {
+    enable = true;
+    profiles.default.extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      bitwarden
+    ];
+  };
+
+  programs.firefox = {
+    enable = true;
+    profiles.default.extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+      bitwarden
+    ];
+  };
+  programs.neovim.enable = true;
+  programs.yt-dlp.enable = true;
 }
 
 
