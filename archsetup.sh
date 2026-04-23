8c63b5e257c6fe3f0f4c750579c7a26d4e8c6260
jonathan earl <earl.jonathan@gmail.com>
2026-04-23 09:34:13 -0700

    refactor(setup): migrate Makefile logic to standalone shell scripts
    - Replace complex Makefile setup and `setup.sh` with dedicated `archsetup.sh`, `asahisetup.sh`, and `utils.sh`
    - Extract shared logic (symlinking, secret creation, repo cloning) into shell functions in `utils.sh`
    - Update `.editorconfig` to use 8-character tab indentation
    - Streamline Arch and Asahi script logic for package installation and system configuration
    - Remove deprecated system configuration blocks from the Makefile

---
+++ archsetup.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# https://raw.githubusercontent.com/whattheearl/dotfiles/master/archsetup.sh
+
+source ./utils.sh
+
+echo 'installing packages...'
+command -v yay > /dev/null 2>&1 ||
+	(sudo pacman -S --needed --noconfirm base-devel &&
+	git clone https://aur.archlinux.org/yay.git ~/wte/yay &&
+	cd ~/wte/yay &&
+	makepkg -si)
+
+yay --noconfirm --needed -S \
+	age \
+	alacritty \
+	bluez \
+	bluez-utils \
+	bun \
+	docker \
+	docker-buildx \
+	docker-compose \
+	fd \
+	feh \
+	flameshot \
+	fzf \
+	gcc \
+	htop \
+	jq \
+	lazygit \
+	librewolf-bin \
+	llama.cpp-vulkan \
+	man \
+	mpv \
+	neovim \
+	nvm \
+	rbw \
+	ripgrep \
+	rsync \
+	spotify \
+	sqlite \
+	steam \
+	tldr \
+	ttf-firacode-nerd \
+	typescript-language-server \
+	xclip \
+	xorg-xinput \
+	xorg-xrandr \
+	xorg-xset \
+	ytdl \
+	zsh
+
+echo 'create secrets...'
+createSecrets()
+
+echo 'symlink dotfiles...'
+symlinkDotfiles()
+ln -sf "${PWD}/alacritty/alacritty.ws.toml" "${HOME}/.config/alacritty/alacritty.toml"
+
+echo 'config network...'
+nmcli connection modify Wired\ connection\ 1 ipv4.dns "10.0.0.1"
+nmcli connection show Wired\ connection\ 1 | grep ipv4.dns:
+
+echo 'clone repos...'
+cloneRepos()
+
+echo 'change shell...'
+changeShell()
---
+++ utils.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+function symlinkDotfiles() {
+	git clone https://github.com/whattheearl/dotfiles "${HOME}/wte/dotfiles" 2>/dev/null || true
+	cd "${HOME}/wte/dotfiles"
+	ln -sf "${PWD}/bunfig.toml" "${HOME}/.bunfig.toml"
+	ln -sf "${PWD}/git/gitconfig" "${HOME}/.gitconfig"
+	rmdir "${HOME}/.config/i3" > /dev/null 2>&1 || true
+	ln -sf "${PWD}/i3" "${HOME}/.config"
+	ln -sf "${PWD}/mimeapps.list" "${HOME}/.config"
+	ln -sf "${PWD}/npmrc" "${HOME}/.npmrc"
+	ln -sf "${PWD}/nvim" "${HOME}/.config"
+	ln -sf "${PWD}/ripgreprc" "${HOME}/.ripgreprc"
+	ln -sf "${PWD}/sway" "${HOME}/.config"
+	ln -sf "${PWD}/tmux" "${HOME}/.config"
+	ln -sf "${PWD}/waybar" "${HOME}/.config"
+	ln -sf "${PWD}/wofi" "${HOME}/.config"
+	ln -sf "${PWD}/xorg.conf.d" "${HOME}/.xorg.conf.d"
+	ln -sf "${PWD}/xprofile" "${HOME}/.xprofile"
+	ln -sf "${PWD}/Xresources" "${HOME}/.Xresources"
+	ln -sf "${PWD}/zsh/zshrc" "${HOME}/.zshrc"
+}
+
+function createSecrets() {
+	rbw config set email earl.jonathan@gmail.com
+	mkdir -p ~/.ssh
+	rbw get secrets > ~/.secrets
+	rbw get ssh-private > ~/.ssh/id_ed25519
+	rbw get ssh-public > ~/.ssh/id_ed25519.pub
+	sudo -u ${USER} chmod 600 ~/.secrets
+	sudo -u ${USER} chmod 600 ~/.ssh/id_ed25519
+	sudo -u ${USER} chmod 644 ~/.ssh/id_ed25519.pub
+	sudo -u ${USER} chmod 700 ~/.ssh
+}
+
+function cloneRepos() {
+	[ -d ~/wte/dotfiles ] || git clone git@github.com:whattheearl/dotfiles ~/wte/dotfiles
+	[ -d ~/wte/notes ] || git clone wteos.wte.sh:notes ~/wte/notes
+	[ -d ~/wte/ai ] || (git clone wteos.wte.sh:ai ~/wte/ai && cd ~/wte/ai && bun install && make install)
+	[ -d ~/wte/tracker ] || git clone wteos.wte.sh:tracker ~/wte/tracker
+}
+
+function changeShell() {
+	chsh -s /bin/sh
+}
